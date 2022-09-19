5235dda782109a8b85edf7b70dd49b20bc9e01e9
gokulsivan <gokulsivan24@gmail.com>
2022-09-19 14:56:32 +0530

    test file

--- routes/aliens.js
+++ routes/aliens.js
@@ -10,10 +10,20 @@ router.get('/',async(req,res)=>{
         res.send('Error' + err)
     }
 })
+
+router.get('/:id',async(req,res)=>{
+    try{
+        const alien = await Alien.findById(req.params.id)
+        res.json(alien)
+    }catch(err){
+        res.send('Error' + err)
+    }
+})
+
 router.post('/', async(req,res)=>{
     const alien = new Alien({
         name : req.body.name,
-        tech: req.body.sub
+        tech: req.body.tech
     })
     try{
         const a1 = await alien.save()
@@ -23,5 +33,25 @@ router.post('/', async(req,res)=>{
     }        
     
 })
+router.patch('/:id', async(req,res)=> {
+    try{
+        const alien = await Alien.findById(req.params.id)
+        alien.sub = req.body.sub
+        const a1 = await alien.save()
+        res.json(a1)
+    }catch(err){   
+        res.send('Error')
+    }
+})
 
+router.delete('/:id', async(req,res)=> {
+    try{
+        const alien = await Alien.findById(req.params.id)
+        const a1 = await alien.delete()
+        res.json(a1)
+    }catch(err){
+        res.send('Error')
+    }
+})
+// router.delete('/', async(req,res)=> {})
 module.exports = router
